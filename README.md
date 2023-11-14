04e61038e184418efdea814ea672101e475baebd
Eyal Erez <oneself@gmail.com>
2023-11-14 09:55:09 +0200

    Getting the basic functionality to work.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# emacsgpt
-Interact with ChatGPT inside emacs
+# EmacsGPT
+
+This is an emacs mode that can be used to interact with ChatGPT from inside
+emacs. To use, follow these steps:
+
+1. Set the OpenAI API key. In `.zshrc` or `.bashrc` (this can also be done in emacs):
+
+    export OPENAI_API_KEY=<API_KEY>
+
+2. Add the following to your `~/.emacs.d/init.el`:
+
+    ;; Ensure request.el is installed, this can be done via (use-package
+    request) or manually.
+    (load "<PATH>/emacsgpt.el")        ; Load package
+    (setq emacsgpt-api-model "gpt-4")  ; Set API model to use (default is "gpt-3.5-turbo")
+    (global-emacsgpt-mode t)           ; Turn minor mode on globally
+
+3. Once in any buffer use one of the following functions to interact with ChatGPT:
+   - `emacsgpt-eval-region` (`C-c c r`) : Send active region.
+   - `emacsgpt-eval-paragraph` (`C-c c r`) : Send current paragraph.
+   - `emacsgpt-eval-message` (`C-c c r`) : Send a custom message.
--- emacsgpt.el
+++ emacsgpt.el
@@ -1,6 +1,6 @@
 
 (require 'request)
-
+(require 'subr-x)
 
 (setq debug-on-error t)
 (setq url-debug t)
@@ -8,11 +8,9 @@
 (defvar emacsgpt-api-key nil
   "API key for OpenAI. Set this variable or the environment variable OPENAI_API_KEY.")
 
-;;(defvar emacsgpt-api-model "gpt-4"
 (defvar emacsgpt-api-model "gpt-3.5-turbo"
   "Model name for OpenAI API.")
 
-;;(defvar emacsgpt-api-endpoint (concat "https://api.openai.com/v1/engines/" emacsgpt-api-model "/completions")
 (defvar emacsgpt-api-endpoint (concat "https://api.openai.com/v1/chat/completions")
   "Endpoint for OpenAI API.")
 
@@ -21,12 +19,12 @@
   (or emacsgpt-api-key
       (getenv "OPENAI_API_KEY")))
 
-(defun emacsgpt-log (msg)
+(defun emacsgpt-log (content)
+  "Log CONTENT to the *emacsgpt log* buffer."
   (with-current-buffer (get-buffer-create "*emacsgpt log*")
-    (let ((inhibit-read-only t))
-      (goto-char (point-max))
-      (insert (msg)))))
-
+    (goto-char (point-max))  ; Move to the end of the buffer
+    (insert content)         ; Insert the content
+    (insert "\n")))          ; Add a newline for readability
 
 (defun emacsgpt-query (input)
   "Send INPUT to the OpenAI API and return the response."
@@ -47,20 +45,19 @@
               (lambda (&rest args &key error-thrown &allow-other-keys)
                 (message "Got error: %S" error-thrown))))))
 
-
 (defun emacsgpt-query (input)
   "Send INPUT to the OpenAI API and return the response."
   ;; Construct headers
   (with-current-buffer (switch-to-buffer-other-window (get-buffer-create "*emacsgpt*"))
     (let ((inhibit-read-only t))
       (goto-char (point-max))
-      (insert (format "\n\n------------------------------------- INPUT ------------------------------------\n%s\n" input))  ; Insert input
+      (insert (format "\n\n------------------------------------- INPUT ------------------------------------\n\n%s\n" input))  ; Insert input
       (inferior-emacsgpt-mode)))
   (let ((headers `(("Content-Type" . "application/json")
                    ("Authorization" . ,(concat "Bearer " (emacsgpt-api-key-get)))))
         (request-data (json-encode `(("model" . ,emacsgpt-api-model)
                                      ("messages" . ((("role" . "user") ("content" . ,input))))))))
-    (log (format "Request url: %s, headers: %s, data: %s" emacsgpt-api-endpoint headers request-data))
+    (emacsgpt-log (format "Request url: %s, headers: %s, data: %s" emacsgpt-api-endpoint headers request-data))
     (request emacsgpt-api-endpoint
       :method "POST"
       :headers headers
@@ -87,31 +84,36 @@
     (with-current-buffer (get-buffer-create "*emacsgpt*")
       (let ((inhibit-read-only t))
         (goto-char (point-max))
-        (insert (format "------------------------------------- OUTPUT -----------------------------------\n\n%s\n" output))   ; Insert output
+        (insert (format "\n\n------------------------------------- OUTPUT -----------------------------------\n\n%s\n" output))
+        (goto-char (point-max))
         (inferior-emacsgpt-mode)))))
 
 
 (defun emacsgpt-eval-region (start end)
-  "Evaluate the region from START to END using the Emacsgpt API."
+  "Evaluate the region from START to END using the OpenAI API."
   (interactive "r")
-  (let* ((input (buffer-substring-no-properties start end)))
-    (emacsgpt-query input)))
-
-
-;; (defun emacsgpt-eval-region (start end)
-;;   "Evaluate the region from START to END using the Emacsgpt API."
-;;   (interactive "r")
-;;   (let* ((input (buffer-substring-no-properties start end))
-;;          (messages `[ (("role" . "user") ("content" . ,input)) ]))
-;;     (emacsgpt-query messages 'emacsgpt-handle-response)))
+  (let* ((region-input (string-trim (buffer-substring-no-properties start end)))
+         (user-input (read-string "Message: "))
+         (combined-input (concat region-input "\n\n" user-input)))
+    (if (string= "" (string-trim combined-input))
+        (message "Input is empty.")
+      (emacsgpt-query combined-input))))
 
 (defun emacsgpt-eval-paragraph ()
-  "Evaluate the current paragraph using the Emacsgpt API."
+  "Evaluate the current paragraph using the OpenAI API."
   (interactive)
   (save-excursion  ; Preserve the point's original position
     (let* ((start (progn (backward-paragraph) (point)))  ; Find start of the paragraph
            (end (progn (forward-paragraph) (point))))    ; Find end of the paragraph
       (emacsgpt-eval-region start end))))
+
+(defun emacsgpt-eval-message ()
+  "Send a message to OpenAI API."
+  (interactive)
+  (let* ((user-input (read-string "Message: ")))
+    (if (string= "" (string-trim user-input))
+        (message "Input is empty.")
+      (emacsgpt-query user-input))))
 
 (define-minor-mode emacsgpt-mode
   "A minor mode to interact with OpenAI's Emacsgpt."
@@ -119,6 +121,7 @@
   :keymap (let ((map (make-sparse-keymap)))
             (define-key map (kbd "C-c c r") 'emacsgpt-eval-region)
             (define-key map (kbd "C-c c p") 'emacsgpt-eval-paragraph)
+            (define-key map (kbd "C-c c m") 'emacsgpt-eval-message)
             map))
 
 (define-derived-mode inferior-emacsgpt-mode fundamental-mode "Inferior Emacsgpt"
@@ -127,8 +130,8 @@
   (setq-local read-only t))
 
 (defvar inferior-emacsgpt-font-lock-keywords
-  '(("\\(.*INPUT.*\\)" . font-lock-function-name-face)
-    ("\\(.*OUTPUT.*\\)" . font-lock-variable-name-face)))
+  '((".*INPUT.*" . font-lock-function-name-face)
+    (".*OUTPUT.*" . font-lock-variable-name-face)))
 
 (defun emacsgpt--turn-on ()
   "Turn on `emacsgpt-mode` in the current buffer if appropriate."
