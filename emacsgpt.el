a565268d5fb177cb2fa32bd3cdc2fd46d76f7fdc
Eyal Erez <oneself@gmail.com>
2023-11-19 17:40:51 +0200

    Removed spinner since it doesn't work.

--- emacsgpt.el
+++ emacsgpt.el
@@ -1,7 +1,6 @@
 
 (require 'request)
 (require 'subr-x)
-(require 'spinner)
 
 ;(setq debug-on-error t)
 
@@ -21,11 +20,6 @@
 (defvar emacsgpt-print-line-num 5
   "Number of lines to print in the *emacsgpt* buffer.")
 
-(defvar-local emacsgpt--spinner nil)
-
-(defconst emacsgpt--lighter
-  '(" emacsgpt" (:eval (spinner-print emacsgpt--spinner))))
-
   ;;;;;;;;;;;;;;;
  ;; UTILITIES ;;
 ;;;;;;;;;;;;;;;
@@ -42,67 +36,43 @@
     (insert content)         ; Insert the content
     (insert "\n")))          ; Add a newline for readability
 
-(defun emacsgpt--start-spinner ()
-  "Create and start a spinner in the *chatgpt* buffer."
-  ;(with-current-buffer (get-buffer-create "*chatgpt*")
-    (unless emacsgpt--spinner
-      (setq emacsgpt--spinner (spinner-create 'moon 10)))
-    (spinner-start emacsgpt--spinner)
-    (message "spinner started"))
-;)
-
-(defun emacsgpt--stop-spinner ()
-  "Stop the spinner in the *chatgpt* buffer."
-  ;(with-current-buffer (get-buffer-create "*chatgpt*")
-    (when emacsgpt--spinner
-      (spinner-stop emacsgpt--spinner)
-      (setq emacsgpt--spinner nil)
-      (message "spinner stopped")))
-;)
-
-;(emacsgpt--start-spinner)
-;(emacsgpt--stop-spinner)
-
-
   ;;;;;;;;;;
  ;; CHAT ;;
 ;;;;;;;;;;
 
-(defun emacsgpt-print-input (input user-input)
-  "Print the INPUT and USER-INPUT to emacsgpt buffer."
+(defun emacsgpt-print-input (context input)
+  "Print the CONTEXT and INPUT to emacsgpt buffer."
   (with-current-buffer (switch-to-buffer-other-window (get-buffer-create "*emacsgpt*"))
     (let ((inhibit-read-only t))
       (goto-char (point-max))
-      (when (not (string-empty-p input))  ; check if input is not empty
-        (let* ((input-lines (split-string input "\n"))
+      (when (not (string-empty-p context))  ; check if context is not empty
+        (let* ((input-lines (split-string context "\n"))
                (first-lines (cl-subseq input-lines 0 (min emacsgpt-print-line-num (length input-lines))))
                (remaining-lines (- (length input-lines) emacsgpt-print-line-num)))
-          (insert (format "\n\n------------------------------------- INPUT ------------------------------------\n\n%s\n"
+          (insert (format "\n\n------------------------------------ CONTEXT -----------------------------------\n\n%s\n"
                           (string-join first-lines "\n")))
           (when (> remaining-lines 0)
             (insert (format "\n[%d more lines]\n" remaining-lines)))))
-      (insert (format "\n---------------------------------- USER INPUT ---------------------------------\n\n%s\n"
-                      user-input)))
+      (insert (format "\n------------------------------------- INPUT ------------------------------------\n\n%s\n"
+                      input)))
     (inferior-emacsgpt-mode)))
 
 
-(defun emacsgpt-query (input user-input)
-  "Send INPUT and USER-INPUT to the OpenAI API and return the response."
+(defun emacsgpt-query (context input)
+  "Send CONTEXT and INPUT to the OpenAI API and return the response."
   ;; Construct headers and request data
-  (let* ((input-tr (string-trim input))
-         (user-input-tr (string-trim user-input))
-         (combined-input (concat input-tr "\n\n" user-input-tr))
+  (let* ((context-tr (string-trim context))
+         (input-tr (string-trim input))
+         (combined-input (concat context-tr "\n\n" input-tr))
          (headers `(("Content-Type" . "application/json")
                     ("Authorization" . ,(concat "Bearer " (emacsgpt-api-key-get)))))
          (request-data (json-encode `(("model" . ,emacsgpt-api-model)
                                       ("messages" . ((("role" . "user") ("content" . ,combined-input))))))))
-    (if (or (string-empty-p input) (string-empty-p user-input))
+    (if (and (string-empty-p context) (string-empty-p input))
         (message "Input is empty")
       (emacsgpt-log (format "Request url: %s, headers: %s, data: %s" emacsgpt-api-chat-endpoint headers request-data))
-      ;; Print the input
-      (emacsgpt-print-input input-tr user-input-tr)
-      ;; Start the spinner
-      (emacsgpt--start-spinner)
+      ;; Print the context
+      (emacsgpt-print-input context-tr input-tr)
       (request emacsgpt-api-chat-endpoint
         :method "POST"
         :headers headers
@@ -110,11 +80,9 @@
         :parser 'json-read
         :success (cl-function
                   (lambda (&key data &allow-other-keys)
-                    (emacsgpt--stop-spinner)
                     (emacsgpt-handle-response data)))
         :error (cl-function
                 (lambda (&rest args &key error-thrown &allow-other-keys)
-                  (emacsgpt--stop-spinner)
                   (emacsgpt-log error-thrown)
                   (message "Got error: %S" error-thrown)))))))
 
@@ -140,9 +108,9 @@
 (defun emacsgpt-eval-region (start end)
   "Evaluate the region from START to END using the OpenAI API."
   (interactive "r")
-  (let* ((input (buffer-substring-no-properties start end))
-         (user-input (read-string "Message: ")))
-    (emacsgpt-query input user-input)))
+  (let* ((context (buffer-substring-no-properties start end))
+         (input (read-string "Message: ")))
+    (emacsgpt-query context input)))
 
 (defun emacsgpt-eval-paragraph ()
   "Evaluate the current paragraph using the OpenAI API."
@@ -158,11 +126,40 @@
   ;; Use `point-min` and `point-max` to get the start and end of the buffer
   (emacsgpt-eval-region (point-min) (point-max)))
 
+(defun emacsgpt-eval-org (&optional levels)
+  "Evaluate the current Org mode heading and its content using the OpenAI API.
+Optional argument LEVELS indicates the number of parent org levels to include."
+  (interactive "p")
+  (if (eq major-mode 'org-mode)
+      (save-excursion
+        (while (and (> levels 0) (org-up-heading-safe)) ; Go up by 'levels' number of headings
+          (setq levels (1- levels)))
+        (let ((start (point)))
+          (outline-end-of-subtree)
+          (emacsgpt-eval-region start (point))))
+    (message "Not in org-mode")))
+
+(defun emacsgpt-eval-org-0 ()
+  "Evaluate the current Org mode heading and its content using the OpenAI API."
+  (interactive)
+  (emacsgpt-eval-org 0))
+
+(defun emacsgpt-eval-org-1 ()
+  "Evaluate the current Org mode heading and its content including one level of parent org levels using the OpenAI API."
+  (interactive)
+  (emacsgpt-eval-org 1))
+
+(defun emacsgpt-eval-org-2 ()
+  "Evaluate the current Org mode heading and its content including two levels of parent org levels using the OpenAI API."
+  (interactive)
+  (emacsgpt-eval-org 2))
+
+
 (defun emacsgpt-eval-message ()
   "Send a message to OpenAI API."
   (interactive)
-  (let* ((user-input (read-string "Message: ")))
-      (emacsgpt-query "" user-input)))
+  (let* ((context (read-string "Message: ")))
+      (emacsgpt-query "" context)))
 
   ;;;;;;;;;;
  ;; MODE ;;
@@ -170,27 +167,27 @@
 
 (define-minor-mode emacsgpt-mode
   "A minor mode to interact with OpenAI's Emacsgpt."
-  :lighter emacsgpt--lighter
   :keymap (let ((map (make-sparse-keymap)))
             (define-key map (kbd "C-c c r") 'emacsgpt-eval-region)
             (define-key map (kbd "C-c c b") 'emacsgpt-eval-buffer)
             (define-key map (kbd "C-c c p") 'emacsgpt-eval-paragraph)
             (define-key map (kbd "C-c c m") 'emacsgpt-eval-message)
+            (define-key map (kbd "C-c c 0") 'emacsgpt-eval-org-0)
+            (define-key map (kbd "C-c c 1") 'emacsgpt-eval-org-1)
+            (define-key map (kbd "C-c c 2") 'emacsgpt-eval-org-2)
             map))
 
 (define-derived-mode inferior-emacsgpt-mode markdown-mode "Inferior Emacsgpt"
   "Major mode for Emacsgpt interaction."
   ;; Combine markdown-mode's font-lock keywords with custom ones
   (setq-local font-lock-defaults `(,(append markdown-mode-font-lock-keywords
-                                            '((".*INPUT.*" . font-lock-function-name-face)
-                                              (".*OUTPUT.*" . font-lock-variable-name-face)))))
+                                            '((".*CONTEXT.*" . font-lock-function-name-face)
+                                              (".*INPUT.*"   . font-lock-function-name-face)
+                                              (".*OUTPUT.*"  . font-lock-variable-name-face)))))
   ;; Make the buffer read-only
   (setq-local buffer-read-only t)
   ;; Set local key binding for 'q' to quit-window
   (local-set-key (kbd "q") 'quit-window))
-
-;; Set the lighter for the mode line
-(add-to-list 'minor-mode-alist '(inferior-emacsgpt-mode emacsgpt--lighter))
 
 (defun emacsgpt--turn-on ()
   "Turn on `emacsgpt-mode` in the current buffer if appropriate."
